1642d412f9f1c953f0cf7f4be8cb44f1eb05a5f0
Ender Mat <97897154+Ender04@users.noreply.github.com>
2022-05-01 20:13:21 +0200

    Create GatheringLib.sol

---
+++ GatheringLib.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.13;
+
+
+
+
+
+
+
+import {sharedDefinitions} from "./sharedstructs.sol";
+
+
+
+library GatheringLib{
+
+ 
+    // placeholder for now. Gathering Iron/stone/wood makes more sense at the unity side of things via a privileged contract call at the base contract level. 
+
+        function GatheringSkillCast() external{
+
+
+           
+
+
+        }
+
+
+
+
+    
+}
+
+
+
